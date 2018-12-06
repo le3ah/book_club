96325903a836598304e0c0eb7e02dafa8aa5d5f5
Leah Miller <lkmiller.87@gmail.com>
2018-12-05 19:13:21 -0700

    Adds passing tests for link to book show page

--- spec/features/user_sees_show_page_spec.rb
+++ spec/features/user_sees_show_page_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe 'user show page' do
   it 'user can see book details' do
     book_1 = Book.create(title: "To Kill a Mockingbird", pages: 281, year: 1960,)
-    author_1 = book_1.authors.create(name: "Harper Lee")
-    author_2 = book_1.authors.create(name: "Jimmy Dean")
+    book_1.authors.create(name: "Harper Lee")
+    book_1.authors.create(name: "Jimmy Dean")
     user_1 = User.create(name: "MicJagger")
     user_2 = User.create(name: "Sal")
     user_3 = User.create(name: "Megan")
-    review_1 = user_1.reviews.create(title: "Great Read!", description: "Wow!! Pleasantly surprised.", rating: 5, book: book_1)
-    review_2 = user_2.reviews.create(title: "Hey", description: "Wow!! Not Great", rating: 2, book: book_1)
-    review_3 = user_3.reviews.create(title: "Cool", description: "Givita1", rating: 1, book: book_1)
+    user_1.reviews.create(title: "Great Read!", description: "Wow!! Pleasantly surprised.", rating: 5, book: book_1)
+    user_2.reviews.create(title: "Hey", description: "Wow!! Not Great", rating: 2, book: book_1)
+    user_3.reviews.create(title: "Cool", description: "Givita1", rating: 1, book: book_1)
 
 
     visit book_path(book_1)
@@ -29,4 +29,22 @@ describe 'user show page' do
     end
     # expect(page).to have_content(book_1.avg_rating)
   end
+
+    it "links to corresponding book show page" do
+      book_1 = Book.create(title: "To Kill a Mockingbird", pages: 281, year: 1960,)
+      book_1.authors.create(name: "Harper Lee")
+      book_1.authors.create(name: "Jimmy Dean")
+      user_1 = User.create(name: "MicJagger")
+      user_2 = User.create(name: "Sal")
+      user_3 = User.create(name: "Megan")
+      user_1.reviews.create(title: "Great Read!", description: "Wow!! Pleasantly surprised.", rating: 5, book: book_1)
+      user_2.reviews.create(title: "Hey", description: "Wow!! Not Great", rating: 2, book: book_1)
+      user_3.reviews.create(title: "Cool", description: "Givita1", rating: 1, book: book_1)
+
+
+      visit books_path
+
+      click_link "#{book_1.title}"
+      expect(current_path).to eq(book_path(book_1))
+    end
 end
