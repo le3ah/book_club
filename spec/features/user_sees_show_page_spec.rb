3906fa874d1d748fe961e4ff0b6bf379bb6f3657
Leah Miller <lkmiller.87@gmail.com>
2018-12-05 19:33:41 -0700

    Adds passing tests for links to book show page; started work on author show page

--- spec/features/user_sees_show_page_spec.rb
+++ spec/features/user_sees_show_page_spec.rb
@@ -30,21 +30,24 @@ describe 'user show page' do
     # expect(page).to have_content(book_1.avg_rating)
   end
 
-    it "links to corresponding book show page" do
-      book_1 = Book.create(title: "To Kill a Mockingbird", pages: 281, year: 1960,)
-      book_1.authors.create(name: "Harper Lee")
-      book_1.authors.create(name: "Jimmy Dean")
-      user_1 = User.create(name: "MicJagger")
-      user_2 = User.create(name: "Sal")
-      user_3 = User.create(name: "Megan")
-      user_1.reviews.create(title: "Great Read!", description: "Wow!! Pleasantly surprised.", rating: 5, book: book_1)
-      user_2.reviews.create(title: "Hey", description: "Wow!! Not Great", rating: 2, book: book_1)
-      user_3.reviews.create(title: "Cool", description: "Givita1", rating: 1, book: book_1)
+  it "links to corresponding book show page" do
+    book_1 = Book.create(title: "To Kill a Mockingbird", pages: 281, year: 1960,)
+    book_1.authors.create(name: "Harper Lee")
+    book_1.authors.create(name: "Jimmy Dean")
 
+    visit books_path
 
-      visit books_path
+    click_link "#{book_1.title}"
+    expect(current_path).to eq(book_path(book_1))
+  end
 
-      click_link "#{book_1.title}"
-      expect(current_path).to eq(book_path(book_1))
-    end
+  # it "links to corresponding author show page" do
+  #   book_1 = Book.create(title: "To Kill a Mockingbird", pages: 281, year: 1960,)
+  #   author_1 = book_1.authors.create(name: "Harper Lee")
+  #
+  #   visit author_path(author_1)
+  #   binding.pry
+  #   click_link "#{author_1.name}"
+  #   expect(current_path).to eq(author_path(author_1))
+  # end
 end
